44917f79a9874560a2513a2f31ece6e1576a2cfe
6teloiv <rosenzweig.violet@gmail.com>
2025-04-30 00:38:35 -0400

    feat: buk

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,93 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="container mx-auto p-4 flex items-center justify-center min-h-screen">
+      <main>
+        <div className="perspective-distant">
+          <div className="transform-3d animate-book aspect-book w-sm">
+            <div
+              id="back-cover"
+              className="w-full h-full -translate-z-8 absolute bg-gradient-to-br from-amber-900 to-amber-950"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div
+              id="spine"
+              className="absolute -translate-z-8 origin-left -rotate-y-90 h-full w-16 bg-gradient-to-bl from-amber-900 to-amber-950 overflow-clip"
+            >
+              <div className="absolute inset-0 noise-large" />
+              <div className="absolute w-full h-4 top-1/6 -translate-y-1/2 outline-amber-400 outline-2 outline-offset-4 bg-amber-400 drop-shadow-amber-900 drop-shadow" />
+              <div className="absolute w-full h-4 top-1/3 -translate-y-1/2 outline-amber-400 outline-2 outline-offset-4 bg-amber-400 drop-shadow-amber-900 drop-shadow" />
+              <div className="absolute w-full h-4 top-1/2 -translate-y-1/2 outline-amber-400 outline-2 outline-offset-4 bg-amber-400 drop-shadow-amber-900 drop-shadow" />
+              <div className="absolute w-full h-4 top-2/3 -translate-y-1/2 outline-amber-400 outline-2 outline-offset-4 bg-amber-400 drop-shadow-amber-900 drop-shadow" />
+              <div className="absolute w-full h-4 top-5/6 -translate-y-1/2 outline-amber-400 outline-2 outline-offset-4 bg-amber-400 drop-shadow-amber-900 drop-shadow" />
+            </div>
+            <div
+              id="pages-right"
+              className="absolute top-2 bottom-2 right-2 -translate-z-8 origin-right rotate-y-90 w-16 bg-gradient-to-bl from-stone-200 to-stone-400"
+            />
+            <div
+              id="pages-top"
+              className="absolute top-2 left-0 right-2 -translate-z-8 origin-top rotate-x-90 h-16 bg-gradient-to-tr from-stone-200 to-stone-400"
+            />
+            <div
+              id="pages-bottom"
+              className="absolute bottom-2 left-0 right-2 -translate-z-8 origin-bottom -rotate-x-90 h-16 bg-gradient-to-tr from-stone-200 to-stone-400"
+            />
+            <div
+              id="page-sticking-out-1"
+              className="absolute top-4 left-1/4 right-0 bottom-0 -translate-z-2 rotate-z-3 bg-gradient-to-br from-stone-200 to-stone-400"
+            />
+            <div
+              id="page-sticking-out-2"
+              className="absolute top-4 left-1/4 right-0 bottom-2 -translate-z-2 rotate-z-1 bg-gradient-to-br from-stone-200 to-stone-400"
+            />
+            <div
+              id="page-sticking-out-3"
+              className="absolute top-2 left-1/4 right-0 bottom-4 translate-z-2 -rotate-z-1 bg-gradient-to-br from-stone-200 to-stone-400"
+            />
+            <div
+              id="page-sticking-out-4"
+              className="absolute top-0 left-1/4 right-0 bottom-4 translate-z-2 -rotate-z-3 bg-gradient-to-br from-stone-200 to-stone-400"
+            />
+            <div
+              id="cover"
+              className="w-full h-full translate-z-8 absolute bg-gradient-to-br from-amber-900 to-amber-950"
+            >
+              <div className="absolute inset-0 noise-large" />
+              <div className="absolute inset-1 border-4 border-amber-400 drop-shadow-amber-900 drop-shadow" />
+              <div className="absolute inset-0 p-8 flex flex-col justify-between">
+                <div>
+                  <p className="font-byline text-xl mb-1 text-amber-400 drop-shadow-amber-900 drop-shadow">
+                    Violet&rsquo;s
+                  </p>
+                  <h1 className="font-headline text-4xl text-amber-400 drop-shadow-amber-900 drop-shadow">
+                    Grotto Bestiary
+                  </h1>
+                </div>
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 size-10 drop-shadow-amber-900 drop-shadow">
+                  <div className="absolute outline-4 outline-amber-950 outline-offset-[calc(var(--spacing)*16)] -translate-y-1/2 bg-radial-[at_25%_25%] from-green-500 via-25% to-green-600 via-green-800 rounded-full size-10 border-green-700 border-2" />
+                  <div className="absolute pt-16 origin-top w-10">
+                    <div className="bg-radial-[at_25%_25%] outline-4 outline-amber-950 outline-offset-[calc(var(--spacing)*5)] from-eternity-100 via-25% to-eternity-200 via-eternity-400 rounded-full size-10 border-eternity-300 border-2" />
+                  </div>
+                  <div className="absolute pt-16 origin-top w-10 -rotate-[120deg]">
+                    <div className="bg-radial-[at_25%_25%] outline-4 outline-amber-950 outline-offset-[calc(var(--spacing)*5)] from-blue-500 via-25% to-blue-600 via-blue-800 rounded-full size-10 rotate-[120deg] border-blue-700 border-2" />
+                  </div>
+                  <div className="absolute pt-16 origin-top w-10 rotate-[120deg]">
+                    <div className="bg-radial-[at_25%_25%] outline-4 outline-amber-950 outline-offset-[calc(var(--spacing)*5)] from-red-400 via-25% to-red-500 via-red-700 rounded-full size-10 -rotate-[120deg] border-red-600 border-2" />
+                  </div>
+                </div>
+                <div>
+                  <p className="font-byline text-lg mb-1 text-amber-400 drop-shadow-amber-900 drop-shadow">
+                    Vol. 1
+                  </p>
+                  <p className="font-byline mb-1 text-amber-400 drop-shadow-amber-900 drop-shadow">
+                    Documenting the grottos, beasts, champions, and wishes of
+                    the world
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
